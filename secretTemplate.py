2bca497320dd2babba1bc507c295e536dd12494f
Michael Cooper <michaelcooper99@hotmail.com>
2019-07-20 15:30:03 -0400

    Added secretTemplate.py as example

---
+++ secretTemplate.py
@@ -0,0 +1,15 @@
+secret = 'bot secret'
+macAdd = "mac address of the server computer"
+admins = {
+    "Discord ID of admin": "Admin Name",
+    "Discord ID of admin": "Admin Name"
+}
+friends = {
+    "Discord ID of user": "User",
+    "Discord ID of user": "User",
+    "Discord ID of user": "User"
+}
+games = [
+    "Game Name",
+    "Game Name"
+]
